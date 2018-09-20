c9d1a0a9e25c8096d42fe905ee84651a5f659cbb
ajilityj <aj@ajilitytechnologies.com>
2018-09-20 01:40:39 -0400

    create loading screen

--- App.js
+++ App.js
@@ -1,29 +1,27 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
+ * Minimalist Weather App
+ * Kudos to https://bit.ly/2vAmRbh
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends React.Component {
+  state = {
+    isLoading: true
+  };
 
-type Props = {};
-export default class App extends Component<Props> {
   render() {
+    const { isLoading } = this.state;
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        {isLoading ? (
+          <Text>Fetching the Weather</Text>
+        ) : (
+          <View>
+            <Text>Minimalist Weather App</Text>
+          </View>
+        )}
       </View>
     );
   }
@@ -32,18 +30,8 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    justifyContent: 'center'
+  }
 });
